9235eb28a22b2be70dc12ae3076bb9d84215472e
Enzo Bossmann <enzombromanus@gmail.com>
2026-03-09 15:05:53 -0300

    aula 2

---
+++ AULA 2/Aula2 exercicios.py
@@ -0,0 +1,8 @@
+seu_nome=input("Digite seu nome:")
+nome_pessoa=input("Informe o nome:")
+
+if seu_nome == nome_pessoa:
+    print("Voces tem o mesmo nome")
+else:
+    print("Voces nao tem o mesmo nome")
+
---
+++ AULA 2/multiplo.py
@@ -0,0 +1,6 @@
+num=int(input("Digite um numero:"))
+n=int(input("Digite o multiplo N:"))
+if num%n==0:
+    print(num,"e multiplo de",n)
+else:
+    print(num,"nao e multiplo de",n)
